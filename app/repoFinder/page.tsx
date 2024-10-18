f24b691c93a5392e541be9923bf02dd60635e65d
parvejg <parvezpg321@gmail.com>
2024-10-18 12:34:15 +0530

    fetch api data

--- app/repoFinder/page.tsx
+++ app/repoFinder/page.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaAngleDown, FaHandPointDown } from "react-icons/fa";
-const RepositoryFinder = () => {
+type Lang = {
+  title: string,
+  value: string
+}
+const RepositoryFinder = async () => {
+  const [selectedLan, setSelectedLan] = useState("")
+  const res = await fetch("https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json")
+  const language:  Lang[] =  await res.json()
+function handleLanguage(e){
+  setSelectedLan(e.target.value)
+}
   return (
     <div className=" flex justify-center items-center align-center h-screen w-full ">
       <div className=" flex flex-col items-center   w-[40%] h-[70%] rounded-2xl">
@@ -13,12 +23,17 @@ const RepositoryFinder = () => {
 w-[70%] h-[60%] bg-white rounded-xl pt-4  mt-5 text-center"
         >
           <div className="flex flex-col w-[100%] items-center h-[90%]">
-            <p className=" flex  justify-around items-center gap-3 justify-center text-xl bg-red-100 mt-2 w-[80%] h-9 text-center align-center cursor-pointer rounded-xl border-solid border-2 border-black">
+            {
+              language.map((lan)=>   <p className=" flex  justify-around items-center gap-3 justify-center text-xl bg-red-100 mt-2 w-[80%] h-9 text-center align-center cursor-pointer rounded-xl border-solid border-2 border-black" onChange={handleLanguage}>
               Select a Language{" "}
               <span>
                 <FaAngleDown className="text-end" />{" "}
               </span>
             </p>
+              
+          )
+            }
+         
             <div className=" flex flex-col items-center justify-center w-[70%] h-[100%] bg-gray-100 rounded-xl mt-5">
               <p className="font-bold">Please select a language</p>
             </div>
